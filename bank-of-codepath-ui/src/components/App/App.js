4a322bc0b846511cc968aa44b4452aa1a2ef984d
shayans66 <35939705+shayans66@users.noreply.github.com>
2021-06-16 14:45:40 -0700

    transactions and transfers

--- bank-of-codepath-ui/src/components/App/App.js
+++ bank-of-codepath-ui/src/components/App/App.js
@@ -2,6 +2,7 @@ import Navbar from "../Navbar/Navbar"
 import Home from "../Home/Home"
 import "./App.css"
 import { useState,useEffect } from "react"
+import axios from 'axios'
 
 export default function App() {
 
@@ -12,8 +13,27 @@ export default function App() {
   const [transfers, setTransfers] = useState('')
 
   useEffect(
-    const func = async () => {
-      
+    () => {
+      const func = async () => {
+        
+        try {
+          let axTransactions = await axios.get('http://localhost:3001/bank/transactions')
+          axTransactions = await axTransactions.data
+          let axTransfers = await axios.get('http://localhost:3001/bank/transfers')
+          axTransfers = await axTransfers.data
+
+          setTransactions(axTransactions)
+          setTransfers(axTransfers)
+
+          console.log(axTransactions);
+
+        }catch(err){
+          setError(err)
+        }
+        
+      }
+      setIsFetching(false)
+      func()
     }
   , [])
 
@@ -26,3 +46,75 @@ export default function App() {
     </div>
   )
 }
+
+
+
+// import Navbar from "../Navbar/Navbar"
+// import Home from "../Home/Home"
+// import "./App.css"
+// import axios from 'axios'
+
+
+// import { useState, useEffect } from "react"
+
+// export default function App() {
+
+//   const [isFetching, setIsFetching] = useState(false)
+
+//   const [transactions, setTransactions] = useState([])
+  
+
+  
+//   useEffect( () => {
+//     const fetchData = (async () => {
+
+//       const t = await axios.get('http://localhost:3001/bank/transactions')
+//       // t = await 
+//       let tt = await t.data.transactions
+//       console.log('tt: ',tt);
+//       setTransactions(tt)
+      
+
+//       console.log('gettrans: ' , transactions);
+
+//     })
+//     fetchData()
+
+//   }, [])
+
+//   async function addTrans(description, category, amount){
+//     let res
+//     try{
+//       res = await axios.post('http://localhost:3001/bank/transactions', 
+//         {
+//           "transaction" : 
+//             {
+//                 description,
+//               category,
+//               amount
+//             }
+//         }
+//       )
+//       const data = await res.data
+//       console.log(data);
+//     }
+//     catch(err){
+//       console.log(res);
+//     }
+//   }
+
+
+
+//   const props = {
+//     addTrans,
+//     transactions
+//   }
+//   console.log('props: ',props);
+
+//   return (
+//     <div className="App">
+//       <Navbar props={isFetching} props={setIsFetching} />
+//       <Home {...props} />
+//     </div>
+//   )
+// }
