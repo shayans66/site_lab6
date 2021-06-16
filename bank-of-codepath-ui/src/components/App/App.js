f5d6c6ae0f1296498ba0a428879ca8aa47b9e88f
shayans66 <35939705+shayans66@users.noreply.github.com>
2021-06-16 09:31:36 -0700

    router

--- bank-of-codepath-ui/src/components/App/App.js
+++ bank-of-codepath-ui/src/components/App/App.js
@@ -2,10 +2,15 @@ import Navbar from "../Navbar/Navbar"
 import Home from "../Home/Home"
 import "./App.css"
 
+import { useState } from "react"
+
 export default function App() {
+
+  const [isFetching, setIsFetching] = useState(false)
+
   return (
     <div className="App">
-      <Navbar />
+      <Navbar props={isFetching} props={setIsFetching} />
       <Home />
     </div>
   )
