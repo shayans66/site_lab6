0b2e9f8d266b03df0ad0045c04944fe3b0f230a0
shayans66 <35939705+shayans66@users.noreply.github.com>
2021-06-16 14:39:55 -0700

    stuff

--- bank-of-codepath-ui/src/components/App/App.js
+++ bank-of-codepath-ui/src/components/App/App.js
@@ -1,8 +1,24 @@
 import Navbar from "../Navbar/Navbar"
 import Home from "../Home/Home"
 import "./App.css"
+import { useState,useEffect } from "react"
 
 export default function App() {
+
+  const [isFetching, setIsFetching] = useState(false)
+  const [error, setError] = useState('')
+  const [filterInputValue, setFilter] = useState('')
+  const [transactions, setTransactions] = useState('')
+  const [transfers, setTransfers] = useState('')
+
+  useEffect(
+    const func = async () => {
+      
+    }
+  , [])
+
+
+
   return (
     <div className="App">
       <Navbar />
--- bank-of-codepath-ui/src/components/Navbar/Navbar.js
+++ bank-of-codepath-ui/src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { BrowserRouter,Link } from "react-router-dom"
 import FilterInput from "../FilterInput/FilterInput"
 import codepath from "../../assets/codepath.svg"
 import avatar from "../../assets/avatar.png"
@@ -6,28 +6,30 @@ import "./Navbar.css"
 
 export default function Navbar() {
   return (
-    <nav className="Navbar">
-      <Link className="logo" to="/">
-        <img src={codepath} alt="logo" />
-      </Link>
+    <BrowserRouter>
+      <nav className="Navbar">
+        <Link className="logo" to="/">
+          <img src={codepath} alt="logo" />
+        </Link>
 
-      <div className="search">
-        <FilterInput />
-      </div>
-
-      <div className="user">
-        <div className="notifications">
-          <i className="material-icons md-36">notifications</i>
-          <div className="green-dot" />
+        <div className="search">
+          <FilterInput />
         </div>
-        <div className="avatar">
-          <img src={avatar} alt="avatar" />
-          <div className="info">
-            <p>Person McPerson</p>
-            <span>ID: 12345567</span>
+
+        <div className="user">
+          <div className="notifications">
+            <i className="material-icons md-36">notifications</i>
+            <div className="green-dot" />
+          </div>
+          <div className="avatar">
+            <img src={avatar} alt="avatar" />
+            <div className="info">
+              <p>Person McPerson</p>
+              <span>ID: 12345567</span>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </BrowserRouter>
   )
 }
